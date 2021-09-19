735b2e07c5b70cf58fa1e3ea80a0994848cfcbf4
Erik Svensson <erik@familjensvensson.se>
2021-09-19 23:01:37 +0200

    added badge from travis

--- src/http-common.js
+++ src/http-common.js
@@ -7,11 +7,6 @@ if (process.env.NODE_ENV === 'development') {
   baseUrl = "http://localhost:8080/api";
 }
 
-if (process.env.NODE_ENV === 'e2etest') {
-  // We can even use MongoDB Atlas for testing
-  baseUrl = "http://localhost:8080/api";
-}
-
 console.log("NEV_NODE:", process.env.NODE_ENV);
 console.log("baseURl: ", baseUrl);
 console.log("APIURL: ", process.env.APIURL);
